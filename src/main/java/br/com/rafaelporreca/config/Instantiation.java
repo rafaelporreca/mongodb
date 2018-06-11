0dbe5650a3b5c2771353b79e680d06ee86989792
Rafael Porreca <rafaelporreca@gmail.com>
2018-06-11 12:09:47 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/br/com/rafaelporreca/config/Instantiation.java
@@ -0,0 +1,32 @@
+package br.com.rafaelporreca.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.rafaelporreca.domain.User;
+import br.com.rafaelporreca.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
