56f42a1bcc14c8bec462cd5c9031f9da08ad4efa
Rafael Porreca <rafaelporreca@gmail.com>
2018-06-14 10:10:51 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/br/com/rafaelporreca/config/Instantiation.java
+++ src/main/java/br/com/rafaelporreca/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.rafaelporreca.domain.Post;
 import br.com.rafaelporreca.domain.User;
+import br.com.rafaelporreca.dto.AuthorDTO;
 import br.com.rafaelporreca.repository.PostRepository;
 import br.com.rafaelporreca.repository.UserRepository;
 
@@ -34,11 +35,14 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viajem", "Vou viajar para SP",maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Bom dia todos",maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viajem", "Vou viajar para SP",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Bom dia todos",new AuthorDTO(maria));
+		
+		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		
---
+++ src/main/java/br/com/rafaelporreca/dto/AuthorDTO.java
@@ -0,0 +1,41 @@
+package br.com.rafaelporreca.dto;
+
+import java.io.Serializable;
+
+import br.com.rafaelporreca.domain.User;
+
+public class AuthorDTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String name;
+
+	public AuthorDTO() {
+
+	}
+
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
