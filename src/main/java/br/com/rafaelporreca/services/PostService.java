d881d01305d90119062abef9b09da43939c5380c
Rafael Porreca <rafaelporreca@gmail.com>
2018-06-14 14:45:12 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/rafaelporreca/services/PostService.java
@@ -0,0 +1,27 @@
+package br.com.rafaelporreca.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rafaelporreca.domain.Post;
+import br.com.rafaelporreca.repository.PostRepository;
+import br.com.rafaelporreca.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+}
