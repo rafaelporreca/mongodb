2456eebc15a6e27a31f10997aa064cc6ba5f207e
Rafael Porreca <rafaelporreca@gmail.com>
2018-06-18 14:45:52 -0300

    Consulta simples com @Query

--- src/main/java/br/com/rafaelporreca/repository/PostRepository.java
+++ src/main/java/br/com/rafaelporreca/repository/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.rafaelporreca.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.rafaelporreca.domain.Post;
@@ -11,5 +12,7 @@ import br.com.rafaelporreca.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 
 }
